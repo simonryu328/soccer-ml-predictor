c6ca47235ef4b2d004e6bc05bee41dd56fd8f220
simonryu328 <simonryu328@gmail.com>
2021-11-05 15:12:19 -0700

    scraper and feature engineering

---
+++ ml/feature_eng.py
@@ -0,0 +1,107 @@
+import os
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
+DATA_DIR = os.path.join(BASE_DIR, 'ml','data', 'raw')
+DATA_FEATURED_DIR = os.path.join(BASE_DIR, 'ml', 'data', 'featured')
+
+df = pd.read_csv(os.path.join(DATA_DIR, 'epl_matches.csv'))
+
+df['season'] = df.season.str.split('/').str[0]
+df['date'] = df['date'].apply(datetime.strptime, args=('%d %b %Y', ))
+df[['h_team', 'a_team']] = df['match_name'].str.split(' - ', expand=True)
+df[['h_score', 'a_score']] = df['result'].str.split(':', expand=True)
+df['winner'] = np.where(df.h_score > df.a_score, 'h', np.where(df.a_score > df.h_score, 'a', 'd'))
+#home points made in each match
+df['h_match_points'] = np.where(df['winner'] == 'h', 3 , np.where(df['winner'] == 'd',1, 0))
+#away points made in each match
+df['a_match_points'] = np.where(df['winner'] == 'a', 3 , np.where(df['winner'] == 'd',1, 0))
+
+to_int = ['season', 'h_score', 'a_score']
+to_float = ['h_odd', 'd_odd', 'a_odd']
+
+for col in to_int:
+    df[col] = df[col].astype(int)
+
+for col in to_float:
+    df[col] = df[col].str.replace('-', '0')
+    df[col] = df[col].astype(float)
+
+cols_order = ['season', 'date', 'h_team', 'a_team', 'winner', 'h_score', 'a_score',
+                'h_odd', 'd_odd', 'a_odd', 'h_match_points', 'a_match_points']
+df = df[cols_order]
+
+def create_season_stats(x):
+    season_df = df[(df.season == x.season) & (df.date <= x.date) & ((df.h_team.isin([x.h_team,x.a_team])) | (df.a_team.isin([x.h_team,x.a_team])))]
+    h_df = season_df.groupby(['h_team', 'date']).sum()[['h_match_points', 'h_score', 'a_score']].reset_index()
+    a_df = season_df.groupby(['a_team', 'date']).sum()[['a_match_points', 'a_score', 'h_score']].reset_index()
+    h_df.columns = ['team', 'date','points', 'goals_for', 'goals_against']
+    a_df.columns = ['team', 'date','points', 'goals_for', 'goals_against']
+    
+    full_df = pd.concat([h_df, a_df], ignore_index=True).sort_values(['date'])
+    full_df.loc[full_df.date == x.date, ['points', 'goals_for', 'goals_against']] = 0
+    h_full_df = full_df[full_df.team == x.h_team]
+    a_full_df = full_df[full_df.team == x.a_team]
+    
+    h_full_df['ewm_points'] = h_full_df.points.ewm(span=3, adjust=False).mean().shift(1).fillna(0)
+    a_full_df['ewm_points'] = a_full_df.points.ewm(span=3, adjust=False).mean().shift(1).fillna(0)
+    h_full_df['ewm_goals_for'] = h_full_df.goals_for.ewm(span=3, adjust=False).mean().shift(1).fillna(0)
+    a_full_df['ewm_goals_for'] = a_full_df.goals_for.ewm(span=3, adjust=False).mean().shift(1).fillna(0)
+    h_full_df['ewm_goals_against'] = h_full_df.goals_against.ewm(span=3, adjust=False).mean().shift(1).fillna(0)
+    a_full_df['ewm_goals_against'] = a_full_df.goals_against.ewm(span=3, adjust=False).mean().shift(1).fillna(0)
+    
+    h_ewm_points = float(h_full_df[h_full_df.date == x.date]['ewm_points'])
+    a_ewm_points = float(a_full_df[a_full_df.date == x.date]['ewm_points'])
+    h_ewm_goals_for = float(h_full_df[h_full_df.date == x.date]['ewm_goals_for'])
+    a_ewm_goals_for = float(a_full_df[a_full_df.date == x.date]['ewm_goals_for'])
+    h_ewm_goals_against = float(h_full_df[h_full_df.date == x.date]['ewm_goals_against'])
+    a_ewm_goals_against = float(a_full_df[a_full_df.date == x.date]['ewm_goals_against'])
+    ewm_points = [h_ewm_points, h_ewm_goals_for, h_ewm_goals_against, a_ewm_points, a_ewm_goals_for, a_ewm_goals_against]
+    
+    cum_df = full_df.groupby(['team']).sum()[['points', 'goals_for', 'goals_against']]
+    
+    h_stat = [0,0,0]
+    a_stat = [0,0,0]
+    try:
+        h_stat = cum_df.loc[x.h_team].values
+    except:
+        pass
+    try:
+        a_stat = cum_df.loc[x.a_team].values
+    except:
+        pass
+    
+    return np.concatenate((h_stat, a_stat, ewm_points))
+
+def create_vs_stats(x):
+    # Get all games where two teams played against each other.
+    vs_df = df[(df.date <= x.date) & 
+              (((df.h_team == x.h_team) & (df.a_team == x.a_team)) | ((df.h_team == x.a_team) & (df.a_team == x.h_team)))][['date','h_team', 'a_team', 'h_score', 'a_score']]
+    vs_df.loc[vs_df.date == x.date, ['h_score', 'a_score']] = 0
+               
+    # Swap home and away columns.
+    vs_df.loc[vs_df.h_team == x.a_team, ['h_team', 'a_team', 'h_score', 'a_score']] = vs_df.loc[vs_df.h_team == x.a_team,['a_team','h_team', 'a_score', 'h_score']].values
+
+    h_vs_winrate = vs_df[vs_df.h_score > vs_df.a_score].shape[0]/vs_df.shape[0]
+    a_vs_winrate = vs_df[vs_df.a_score > vs_df.h_score].shape[0]/vs_df.shape[0]
+               
+    return [h_vs_winrate, a_vs_winrate]
+
+season_cols = ['ht_pts','ht_goals_for','ht_goals_against','at_pts','at_goals_for','at_goals_against','h_ewm_points', 
+               'h_ewm_goals_for', 'h_ewm_goals_against', 'a_ewm_points', 'a_ewm_goals_for', 'a_ewm_goals_against']
+vs_cols = ['h_vs_winrate', 'a_vs_winrate']
+
+featured_df = df.copy()
+featured_df[season_cols] = pd.DataFrame(
+    featured_df.apply(lambda x: create_season_stats(x), axis=1).to_list(), index=featured_df.index)
+
+featured_df[vs_cols] = pd.DataFrame(
+    featured_df.apply(lambda x: create_vs_stats(x), axis=1).to_list(), index=featured_df.index)
+
+featured_df.to_csv(os.path.join(DATA_FEATURED_DIR, 'epl_matches_featured.csv'), index=False)
+
+
+
+# df.to_csv(os.path.join(DATA_TRANSFOMRED_DIR, 'epl_matches_transformed.csv'), index=False)
---
+++ ml/scraper.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup as bs
+from datetime import date
+import os
+import pandas as pd
+from selenium import webdriver
+import time
+from typing import List
+
+#working directories
+BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
+DATA_DIR = os.path.join(BASE_DIR, 'ml','data', 'raw')
+
+def get_all_urls(num_years: int) -> List[str]:
+    '''
+    Returns the url list from current season to specified number of years ago.
+    '''
+    end_year = int(date.today().year)
+    start_year = end_year - num_years
+
+    # range of seasons to scrape
+    seasons = list(reversed(range(start_year,end_year)))
+
+    results_url = '/results/'
+    root_url = 'https://www.oddsportal.com/soccer/england/premier-league'
+    first_url = root_url + results_url
+    #all other season url
+    seasons_url = [root_url + '-' + str(season) + '-' + str(season + 1) + results_url for season in seasons]
+    #complete url list to be scraped
+    all_urls = [first_url] + seasons_url
+
+    return all_urls
+
+def get_text(col):
+    '''
+    Returns the text of the column.
+    '''
+    try:
+        result = col.text
+    except:
+        result = None
+    return result
+
+if __name__ == '__main__':
+    num_years = 10
+    driver = webdriver.Chrome('./ml/chromedriver')
+    df = pd.DataFrame()
+    
+    for url in get_all_urls(num_years):
+        driver.get(url)
+        html = driver.page_source
+        soup = bs(html, 'html.parser', from_encoding="utf-8")
+
+        while True:
+            prev_page = soup.find_all( 'span', attrs = {'class' : 'active-page'})[0].text
+
+            for col in soup.find_all('tr', attrs = {'deactivate'}):
+                df = df.append(
+                    {
+                        'season' : soup.find_all('span', attrs = {'class' : 'active'})[1].text,
+                        'date' : col.findPreviousSibling(attrs = {'center nob-border'}).text[0:-6],
+                        'match_name' : col.find('td', attrs = {'class' : 'name table-participant'}).text.replace('\xa0', ''),
+                        'result' : col.find('td', attrs = {'class' : 'center bold table-odds table-score'}).text,
+                        'h_odd' : get_text(col.find('td', attrs = {'class' : "odds-nowrp"})),
+                        'd_odd' : get_text(col.find('td', attrs = {'class' : "odds-nowrp"}).findNext( attrs = {'class' : "odds-nowrp"})),
+                        'a_odd' : get_text(col.find('td', attrs = {'class' : "odds-nowrp"}).findNext( attrs = {'class' : "odds-nowrp"}).findNext( attrs = {'class' : "odds-nowrp"}))
+                    }, ignore_index=True
+                )
+
+            # Go to next page 
+            element = driver.find_element_by_partial_link_text('»')
+            driver.execute_script("arguments[0].click();", element)
+            time.sleep(2)
+
+            # Reload beatifulsoup
+            html = driver.page_source
+            soup = bs(html, 'html.parser', from_encoding="utf-8")
+            #get new page number
+            new_page = soup.find_all('span', attrs = {'class' : 'active-page'})[0].text
+
+            if prev_page != new_page:
+                continue
+            else:
+                break
+
+    driver.quit()
+    df.to_csv(os.path.join(DATA_DIR, 'epl_matches.csv'), index = False)
